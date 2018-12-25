f3f265f876c1322b90df5cb2b15329f4a84b10c6
Karli Juhkam <karli.juhkam@gmail.com>
2018-12-25 17:58:40 +0200

    puhastamine

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -15,21 +15,6 @@ export class MainComponent implements OnInit {
   
 
   ngOnInit() {
-    // this.getLocation();
   }
 
-//   public getLocation() {
-//     /// locate the user
-//     if (navigator.geolocation) {
-//        navigator.geolocation.getCurrentPosition(position => {
-//         this.lat = position.coords.latitude;
-//         this.lng = position.coords.longitude;
-//         console.log('position is: LNG:'+this.lng+' LAT: '+this.lat)
-//       });
-
-//     } else {
-//       console.log('Geolocation is not working');
-//       window.alert("Geolocation is not supported by this browser or is not allowed.");
-//       }
-//   }
 }
